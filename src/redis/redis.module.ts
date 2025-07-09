afa216faf3f6ebc200ffe8023d9bb1a19d709805
Adenuga Willy <adenugawilly431@gmail.com>
2025-07-09 13:38:24 +0100

    new changes

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -8,7 +8,3 @@ import { RedisService } from './redis.service';
 })
 export class RedisModule {}
 // This module provides a Redis service that can be used globally in the application.
-// The `@Global()` decorator makes the module available throughout the application without needing to import it in every module.
-// The `RedisService` is exported so that it can be injected into other services or controllers.
-// The service is responsible for managing the Redis client connection and can be configured using environment variables.
-// The `RedisService` uses the `ioredis` library for Redis interactions, which is
